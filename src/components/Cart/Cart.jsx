5148030c116ed0beefb8c59f199fa1a79650c347
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-03-28 20:03:34 +0600

    Added => Cart component and calculation

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,18 +5,23 @@ import { faTrash , faArrowRight } from '@fortawesome/free-solid-svg-icons'
 const Cart = ({cart}) => {
     // const cart = props.cart;
     // const {cart} = props;
-    let total = 0;
+    let totalPrice = 0;
+    let totalShipping = 0;
     for(const product of cart){
-        total = total + product.price;
+        totalPrice = totalPrice + product.price;
+        totalShipping = totalShipping + product.shipping;
     }
+    const tax = totalPrice*7/100;
+    const grandTotal = totalPrice + totalShipping + tax;
+
     return (
-        <div>
+        <div className='sticky top-0'>
             <h3 className='text-2xl text-center font-medium mb-5'>Order summery</h3>
             <p className='text-xl'>Selected Item: {cart.length}</p>
-            <p className='text-xl mt-5 mb-5'>Total Price: ${total}</p>
-            <p className='text-xl'>Total Shipping Charge: $10</p>
-            <p className='text-xl mt-5 mb-5'>Tax: $114</p>
-            <h3 className='text-2xl font-medium mb-16'>Grand Total: $175</h3>
+            <p className='text-xl mt-5 mb-5'>Total Price: ${totalPrice}</p>
+            <p className='text-xl'>Total Shipping Charge: ${totalShipping}</p>
+            <p className='text-xl mt-5 mb-5'>Tax: ${tax.toFixed(2)}</p>
+            <h3 className='text-2xl font-medium mb-16'>Grand Total: ${grandTotal.toFixed(2)}</h3>
             <button className='text-xl font-medium bg-red-600 text-white mb-4 w-full p-3 rounded-lg'>Clear Cart <FontAwesomeIcon icon={faTrash} /></button>
             <br />
             <button className='text-xl font-medium bg-yellow-500 text-white w-full p-3 rounded-lg'>Review Order <FontAwesomeIcon icon={faArrowRight}/></button>    
