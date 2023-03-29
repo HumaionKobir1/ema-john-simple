d4af4584302f27a63c23dd7dc2367266aa9665a8
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-03-29 12:35:44 +0600

    Handle quantity from storage to cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,9 +7,20 @@ const Cart = ({cart}) => {
     // const {cart} = props;
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
+
     for(const product of cart){
-        totalPrice = totalPrice + product.price;
+
+        // if(product.quantity === 0){
+        //     product.quantity = 1;
+        // }
+
+        // 2nd way
+        product.quantity = product.quantity || 1;
+
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = totalPrice*7/100;
     const grandTotal = totalPrice + totalShipping + tax;
@@ -17,7 +28,7 @@ const Cart = ({cart}) => {
     return (
         <div className='sticky top-0'>
             <h3 className='text-2xl text-center font-medium mb-5'>Order summery</h3>
-            <p className='text-xl'>Selected Item: {cart.length}</p>
+            <p className='text-xl'>Selected Item: {quantity}</p>
             <p className='text-xl mt-5 mb-5'>Total Price: ${totalPrice}</p>
             <p className='text-xl'>Total Shipping Charge: ${totalShipping}</p>
             <p className='text-xl mt-5 mb-5'>Tax: ${tax.toFixed(2)}</p>
