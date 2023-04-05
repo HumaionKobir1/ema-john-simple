f612a8ca2829944321882378c3e444eb1e54f4fd
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-04-05 20:43:39 +0600

    React Router Setup for Ema John

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import logo from '../../../images/Logo.svg';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <nav className='bg-slate-900 text-white flex justify-between  md:px-40 py-4'>
             <img src={logo} alt="" />
             <div className='flex gap-14 items-center font-medium text-lg p-2'>
-                <a href="/order">Order</a>
-                <a href="/review">Order Review</a>
-                <a href="/inventory">Manage Inventory</a>
-                <a href="/login">Login</a>
+                <Link to="/">Shop</Link>
+                <Link to="/orders">Orders</Link>
+                <Link to="/inventory">Manage Inventory</Link>
+                <Link to="/login">Login</Link>
             </div>
         </nav>
     );
