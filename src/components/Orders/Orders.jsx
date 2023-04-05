a37febaf50710e4e1a3b9087accad8fa2091c25e
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-04-05 21:32:52 +0600

    Extract Cart products from Products data

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -3,15 +3,14 @@ import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 
 const Orders = () => {
-    const products = useLoaderData();
-    console.log(products);
+    const cart = useLoaderData();
     return (
         <div className='shop-container grid grid-cols-12 gap-5'>
             <div className='product-container grid col-span-10 grid-cols-3 gap-10 mt-10 p-5 md:px-20 ml-20 container mx-auto '>
-                <h1>Orders page: {products.length}</h1>
+                <h1>Orders page: {cart.length}</h1>
             </div>
             <div className='col-span-2 bg-orange-200 p-4'>
-                <Cart cart={[]}></Cart>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
--- src/loaders/cartProductsLoader.js
+++ src/loaders/cartProductsLoader.js
@@ -1,8 +1,28 @@
+import { getShoppingCart } from "../utilities/fakedb";
+
 const cartProductsLoader = async () => {
     const loaddedProducts = await fetch('products.json');
     const products = await loaddedProducts.json();
-    console.log(products);
+    
+    const storedCart = getShoppingCart();
+    const savedCart = [];
 
-    return products;
+    for(const id in storedCart){
+        const addedProduct = products.find(pd => pd.id === id);
+        if(addedProduct){
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            savedCart.push(addedProduct);
+        }
+    }
+
+    // if you need to return two thinks
+    
+    // retun [product, saveCart]
+
+    // Another options
+    // return {product, saveCart}
+
+    return savedCart;
 }
 export default cartProductsLoader;
