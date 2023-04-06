6263f78bb5bec2182af9ec22c7096bc29ec6c45b
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-04-06 15:51:22 +0600

    Remove item handler and Make Data consistent across UI

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 import ReviewItem from '../ReviewItem/ReviewItem';
+import { removeFromDb } from '../../utilities/fakedb';
 
 const Orders = () => {
-    const cart = useLoaderData();
+    const savedCart = useLoaderData();
+    const [cart, setCart] = useState(savedCart);
+
+    const handleRemoveFromCart = (id) => {
+        const remaining = cart.filter(product => product.id !== id);
+        setCart(remaining);
+        removeFromDb(id);
+    }
+
     return (
         <div className='shop-container grid grid-cols-12 gap-5'>
             <div className='product-container grid col-span-10 gap-6 mt-10 p-5 md:px-20 ml-20 container mx-auto '>
@@ -12,6 +21,7 @@ const Orders = () => {
                     cart.map(product => <ReviewItem
                     key={product.id}
                     product={product}
+                    handleRemoveFromCart = {handleRemoveFromCart}
                     ></ReviewItem>)
                 }
             </div>
--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TrashIcon } from '@heroicons/react/24/solid'
 
-const ReviewItem = ({product}) => {
+const ReviewItem = ({product, handleRemoveFromCart}) => {
     const {img, id, name, price, quantity} = product;
     return (
         <div className='border-2 rounded-lg p-2 flex justify-between items-center'>
@@ -10,10 +10,10 @@ const ReviewItem = ({product}) => {
                 <div className='gap-1 grid items-center'>
                     <h2 className='text-2xl font-medium'>{name}</h2>
                     <p className='font-medium'>Price: <span className='text-yellow-400'>${price}</span></p>
-                    <p>Shipping Charge: <span></span></p>
+                    <p className='font-normal'>Order Quantity: <span className='text-yellow-400'>{quantity}</span></p>
                 </div>
             </div>
-            <button className='rounded-full bg-red-200 p-2 w-12 h-12'><TrashIcon className="h-8 w-8 text-red-500" /></button>
+            <button onClick={()=>handleRemoveFromCart(id)} className='rounded-full mr-5 bg-red-200 hover:bg-red-100 active:border-t-cyan-300 p-2 w-14 h-14'><TrashIcon className="h-10 w-10 text-red-500" /></button>
         </div>
     );
 };
